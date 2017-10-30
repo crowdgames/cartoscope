7c98350d7669ed3fd09a982a65f9f5f45b683460
Kisalaya <kisalaya@talentpad.com>
2017-10-29 20:02:15 -0400

    removing dups

--- routes/featured.js
+++ routes/featured.js
@@ -22,21 +22,3 @@ router.get('/', function(req, res, next) {
   });
 });
 
-
-router.post('/', function(req, res, next) {
-  projectDB.getFeaturedProject().then(function(data) {
-    var redirectUrl = '/';
-    if(data.length > 0) {
-      var featured = data[0];
-      if (featured.code) {
-        redirectUrl = '/task.html#/?code='+featured.code;
-      } else if(featured.url) {
-        redirectUrl = featured.url;
-      }
-    }
-    res.writeHead(302, {
-      'Location': redirectUrl
-    });
-    res.end();
-  });
-});
