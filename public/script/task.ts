cd804bda367b3546826d652a35bc7dd2fcc2c475
kutub gandhi <kksgandhi@gmail.com>
2020-11-19 16:19:48 -0500

    starting to put matter code in with soapstones

--- public/script/task.ts
+++ public/script/task.ts
@@ -300,8 +300,7 @@ module.controller('taskController', ['$scope', '$location', '$http', 'userData',
       }
 
       vm.randomSoapstone = () => {
-          var soapStones = [["Hi there ", ["Bob", "Jeff", "Sandra"], "it's nice to meet you!", ["falafel", "cheezit", "semaphore"]],
-                            ["I love your ", ["hats", "french toasts", "incredible pecs"], ", they look amazing!", ["option 1", "option 2", "option 3"]]];
+          var soapStones = [["Hi there", ["Bob,", "Jeff,", "Sandra,"], "it's nice to meet you! I love your", ["hat", "french toast", "incredible pecs"]]];
           return soapStones[Math.floor(Math.random() * soapStones.length)];
       }
 
@@ -2749,3 +2748,32 @@ module.controller('geneticTaskController', ['$scope', '$location', '$http', 'use
         };
 
     }]);
+
+// module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+// create an engine
+var engine = Engine.create();
+
+// create a renderer
+var render = Render.create({
+    element: document.body,
+    engine: engine
+});
+
+// create two boxes and a ground
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+// add all of the bodies to the world
+World.add(engine.world, [boxA, boxB, ground]);
+
+// run the engine
+Engine.run(engine);
+
+// run the renderer
+Render.run(render);
