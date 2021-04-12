e7d6e2d7069d85ffa77ee95e631273a2b1a69e2d
Kutub Gandhi <kksgandhi@gmail.com>
2021-04-12 17:49:03 -0500

    hide emoji picker using visibility none

--- public/script/task.ts
+++ public/script/task.ts
@@ -469,6 +469,7 @@ module.controller('taskController', ['$scope', '$location', '$http', 'userData',
           $scope.showPhysics       = true; // the div with the ballpit of emojis
           $scope.showEmoji         = true; // the div with the buttons to select which emoji you want
           document.getElementById("cairn-header")!.innerHTML = "Below are emojis selected by other players.<br><br>You can add an emoji if you want to. Pick one that describes how you're feeling!<br>"
+          document.getElementById("emoji-picker")!.removeAttribute("style");
           Render.run(vm.render);
           vm.hideModal();
       }
@@ -490,7 +491,8 @@ module.controller('taskController', ['$scope', '$location', '$http', 'userData',
       vm.submitEmoji = (submittedEmoji: string) => {
           vm.submittedEmoji = submittedEmoji;
           vm.addEmojiToPhysics(submittedEmoji); // add the emoji to the ballpit
-          $scope.showEmoji = false;
+          document.getElementById("emoji-picker")!.setAttribute("style", "visibility:hidden");
+          // $scope.showEmoji = false;
           // The actual sending of the emoji to the database happens in finishEmojiCreate
       }
 
