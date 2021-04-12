d913e88ac6d201294a7c380fa622b58efb64669c
Kutub Gandhi <kksgandhi@gmail.com>
2021-04-12 16:03:23 -0500

    delete unnecessary shuffle function

--- public/script/task.ts
+++ public/script/task.ts
@@ -21,16 +21,6 @@ module.filter("textBreaks", ['$sce', function ($sce) {
     }
 }]);
 
-Object.defineProperty(Array.prototype, 'shuffle', {
-    value: function() {
-        for (let i = this.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [this[i], this[j]] = [this[j], this[i]];
-        }
-        return this;
-    }
-});
-
 var latCenter: any;
 var lngCenter: any;
 var dZoom = 15;
