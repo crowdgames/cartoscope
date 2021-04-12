b1a86a3310629db11d8d8bb63ba7d4d1f91562fa
Kutub Gandhi <kksgandhi@gmail.com>
2021-04-12 17:22:45 -0500

    comma

--- public/script/task.ts
+++ public/script/task.ts
@@ -454,7 +454,7 @@ module.controller('taskController', ['$scope', '$location', '$http', 'userData',
           $scope.showPhysics       = true; // the div with the ballpit of emojis
           $scope.showEmoji         = true; // the div with the buttons to select which emoji you want
           $scope.showCairnElements = true;
-          document.getElementById("cairn-header")!.innerHTML = "Below are emojis selected by other players.<br><br>You can add an emoji, if you want to. Pick one that describes how you're feeling!<br>"
+          document.getElementById("cairn-header")!.innerHTML = "Below are emojis selected by other players.<br><br>You can add an emoji if you want to. Pick one that describes how you're feeling!<br>"
           Render.run(vm.render);
           vm.hideModal();
       }
