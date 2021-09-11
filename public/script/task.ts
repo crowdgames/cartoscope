cf9558ec853888472d0ad7cdc6c1a4da1d193b4f
Sophie Spatharioti <spatharioti.s@husky.neu.edu>
2021-09-11 00:10:54 -0400

    debugging before images

--- public/script/task.ts
+++ public/script/task.ts
@@ -910,7 +910,11 @@ module.controller('taskController', ['$scope', '$location', '$http', 'userData',
       };
 
       //if in NGS mode, try to get the before image from Google
-      vm.getBeforeImageNGS = function (lat,lon,zoom){
+      vm.getBeforeImageNGS = function (){
+
+        var lat = vm.centerLat;
+        var lon = vm.centerLng;
+        var zoom = vm.dZoom;
         //convert lat,lon to tile coordinates
         var x = Math.floor((lon+180)/360*Math.pow(2,zoom));
         var y = Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom));
