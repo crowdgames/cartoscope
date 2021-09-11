522b6c259f8fa3a035e65fd014549ea513aa76c2
Sophie Spatharioti <spatharioti.s@husky.neu.edu>
2021-09-11 00:38:14 -0400

    Update task.ts

--- public/script/task.ts
+++ public/script/task.ts
@@ -913,15 +913,18 @@ module.controller('taskController', ['$scope', '$location', '$http', 'userData',
       //if in NGS mode, try to get the before image from Google
       vm.getBeforeImageNGS = function (){
 
-        var lat = vm.centerLat;
-        var lon = vm.centerLng;
+        var lat = parseFloat(vm.centerLat);
+        var lon = parseFloat(vm.centerLng);
         var zoom = dZoom;
+        zoom = 18;
         console.log(lat)
         console.log(lon)
         console.log(zoom)
         //convert lat,lon to tile coordinates
         var x = Math.floor((lon+180)/360*Math.pow(2,zoom));
         var y = Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom));
+        var y = Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom));
+        
         console.log(x)
         console.log(y)
         //format the google maps url
