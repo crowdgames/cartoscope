5b4819a24d52fc86a8c1bb9dbcfb8a704eada811
Kutub Gandhi <kksgandhi@gmail.com>
2021-10-25 12:58:20 -0400

    code cleanup

--- NGS_downloader/NGS_downloader.ts
+++ NGS_downloader/NGS_downloader.ts
@@ -37,22 +37,9 @@ let event_name = "ida";
 let lat = 29.61070;
 let lon = -89.85002;
 let zoom = 18;
-/*
-var downloadIfNotText = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
+let layersUrl = `https://storms.ngs.noaa.gov/storms/${event_name}/services/WMTSCapabilities.xml`;
 
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
-};
-
-download('https://www.google.com/images/srpr/logo3w.png', 'google.png', function(){
-  console.log('done');
-});
-*/
-
-let downloadImage = (event_name: string, zoom: number, lat: number, lon: number) => {
+let downloadImage = (layers: string[], zoom: number, lat: number, lon: number) => {
     let layersUrl = `https://storms.ngs.noaa.gov/storms/${event_name}/services/WMTSCapabilities.xml`;
     getNGSLayers(layersUrl, (layers: string[]) => {
         let [x, y] = transformCoordinates(zoom, lat, lon);
@@ -64,54 +51,9 @@ let downloadImage = (event_name: string, zoom: number, lat: number, lon: number)
                     request(cdnUri).pipe(fs.createWriteStream(filename));
             });
         });
-        /*
-        let promises = layers.map(layer => fetch(`https://stormscdn.ngs.noaa.gov/${layer}/${zoom}/${x}/${y}`));
-        Promise.all(promises).then(responses => {
-            responses.forEach((response, idx) => {
-                let layer = layers[idx];
-                if(response.headers.get("content-type") !== "text/html") {
-                    console.log("Hello");
-                    // response!.body!.pipe(fs.createWriteStream(`./imgs/${layer}_${zoom}_${lat}_${lon}.png`))
-                    // response.body.
-                    // console.log(response.json().then(data => console.log(data)));
-                }
-            });
-        });
-        let promises: any[] = [];
-        for (let k = 0; k < layers.length; k++) {
-            const layer = layers[k];
-            var cdnUrl = "https://stormscdn.ngs.noaa.gov/" + layer + "/" + zoom.toString()+"/"+ x.toString()+"/"+ y.toString()
-            promises.push(fetch(cdnUrl));
-        }
-        console.log(promises);
-
-        Promise.all(promises).then(function (responses) {
-            // Get a JSON object from each of the responses
-            return Promise.all(responses.map(function (response) {
-                var content_type = response.headers.get('content-type');
-                console.log(response.headers.get('content-length'));
-                if (content_type == "text/html") {
-                    return false
-                } else {
-                    return true
-                }
-
-            }));
-        }).then(function (data) {
-            // Log the data to the console
-            // You would do something with both sets of data here
-            var ret = {}
-            if (data.indexOf(true) != -1) {
-                ret = {name: event_name, latitude:lat,longitude:lon}
-            } 
-            console.log(ret)
-
-        }).catch(function (error) {
-            // if there's an error, log it
-            console.log(error)
-        });
-        */
     });
 }
 
-downloadImage(event_name, zoom, lat, lon);
+getNGSLayers(layersUrl, (layers: string[]) => {
+    downloadImage(layers, zoom, lat, lon);
+});
