9761fef64b2ab942548708b24f8898d6d33d6b55
Kutub Gandhi <kksgandhi@gmail.com>
2021-11-22 18:42:22 -0500

    remove carto salt from envs.sh

--- vagrant-files/envs.sh
+++ vagrant-files/envs.sh
@@ -2,7 +2,7 @@ export CARTO_DB_USER=converge
 export CARTO_DB_PASSWORD=password                                                     
 export CARTO_DB_NAME=convergeDB                                                       
 export CARTO_MAILER='xyz@gmail.com'                                         
-export CARTO_SALT='$$$$$$$$$$$$$$$$$$$$$$$'
+# export CARTO_SALT is missing since it should be generated with "salt_generator.py"
 export CARTO_PORT=8081
 export CARTO_PORT_SSL=8082
 export CARTO_SSL_KEY='/vagrant/domain.key'
